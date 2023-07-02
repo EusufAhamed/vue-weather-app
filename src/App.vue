6c063a23b6e43248f0caaa10d2bd572bb91e3ab0
Eusuf Ahamed <eusufahmed789@gmail.com>
2023-07-02 23:32:08 +0600

    components added

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,43 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue'
+  import Weather from './components/Weather.vue'
+
+  const city = ref('')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="app">
+    <div class="header container h-100 p-5">
+      <h1 class="mb-5">Weather Application</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar w-50 mx-2">
+          <input v-model="city" type="text" class="input form-control" placeholder="Enter a City">
+        </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <button class="btn-search btn btn-primary">Search <i class="fas fa-search"></i></button>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <br>
+    <Weather :city="city"/>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  body{
+    background-color: #121212;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .header{
+    background-color: #212730;
+    border-radius: 20px;
+    color: #fff;
+    text-align: center;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    margin-top: 5rem;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .btn-search{
+    background-image: linear-gradient(to right, cyan, magenta);
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
